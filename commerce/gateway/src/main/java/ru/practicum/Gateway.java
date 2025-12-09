fea500d1f4a15d66930e825dc6ed334a206aedf5
VitalyL <vetallrssn@yandex.ru>
2025-12-09 15:47:55 +0300

    Sprint 22 (9-gateway-microservices) feat: add gateway service

---
+++ commerce/gateway/src/main/java/ru/practicum/Gateway.java
@@ -0,0 +1,24 @@
+package ru.practicum;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/**
+ * Главный класс сервиса gateway.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@Slf4j
+public class Gateway {
+    /**
+     * Точка входа в приложение.
+     *
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) {
+        log.info("Starting Gateway");
+        SpringApplication.run(Gateway.class, args);
+        log.info("Gateway started");
+    }
+}
